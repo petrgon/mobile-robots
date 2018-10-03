9999fc51863558890526b9d5e95ba2ec2f0783ea
rpi3-2 <rpi3-2@dummy.com>
2018-10-03 18:16:16 +0800

    Multiplying cpp corrected

--- src/multiplying.cpp
+++ src/multiplying.cpp
@@ -5,30 +5,40 @@
 #include "ros/ros.h"
 #include "std_msgs/Int32.h"
 #include <iostream>
+
+bool messageRecieved;
+
+void publish_number(ros::Publisher & pub) {
+    std::cout << "Input value: ";
+    int number = 0;
+    std::cin >> number;
+    std_msgs::Int32 msg;
+    msg.data = number;
+    pub.publish(msg);
+}
+
+
 void number_callback(const std_msgs::Int32::ConstPtr& msg)
 {
-    ROS_INFO("Received [%d]", msg->data);
+    std::cout << "Received " <<  msg->data << "\n";
+    messageRecieved = 1;
 }
 
 
 int main(int argc, char **argv)
 {
+    messageRecieved = 1;
     ros::init(argc, argv, "multiplying");
     ros::NodeHandle node_obj;
     ros::Subscriber sub = node_obj.subscribe("/multiplyed", 1, number_callback);
     ros::Publisher pub = node_obj.advertise<std_msgs::Int32>("/number", 1);
 
-
     while (ros::ok())
     {
-        std::cout << "Input value: ";
-        int number = 0;
-        std::cin >> number;
-
-        std_msgs::Int32 msg;
-        msg.data = number;
-        ROS_INFO("%d", msg.data);
-        pub.publish(msg);
+	if (messageRecieved){
+	    publish_number(pub);
+	    messageRecieved = 0;
+	}
         ros::spinOnce();
     }
 
