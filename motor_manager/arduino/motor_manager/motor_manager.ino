561c09be174a85aeb4dc56da53ab72f460fe7cb3
rpi3-2 <rpi3-2@dummy.com>
2018-10-14 21:54:07 +0800

    Motor manager - signals to motors corrected\neach motor gets its correct signal.

--- motor_manager/arduino/motor_manager/motor_manager.ino
+++ motor_manager/arduino/motor_manager/motor_manager.ino
@@ -1,25 +1,28 @@
 #include <ros.h>
 #include <std_msgs/Int32.h>
-       
+
 #define enA 3
 #define in1 4
 #define in2 5
-
 #define enB 9
 #define in3 6
 #define in4 7
 
 ros::NodeHandle nh;
 
-std_msgs::Int32 left_motor_speed = 0;
-std_msgs::Int32 right_motor_speed = 0;
+int left_motor_speed = 0;
+int right_motor_speed = 0;
+
+bool both_speeds_avalable = false;
 
 void moveLeftMotor(const std_msgs::Int32 &number){
-  left_motor_speed.data = number.data;
+  left_motor_speed = number.data;
+  both_speeds_avalable = false;
 }  
 
 void moveRightMotor(const std_msgs::Int32 &number){
-  right_motor_speed.data = number.data;
+  right_motor_speed = number.data;
+  both_speeds_avalable = true;
 }
 
 ros::Subscriber<std_msgs::Int32> sub_left("left_motor", &moveLeftMotor);   
@@ -44,30 +47,31 @@ void setup()
   digitalWrite(in2, HIGH);  
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
-  digitalWrite(inA, left_motor_speed);
-  digitalWrite(inB, right_motor_speed);
+  analogWrite(enA, left_motor_speed);
+  analogWrite(enB, right_motor_speed);
 }
 
 void loop()
-{         
-  if (left_motor_speed < 0){  
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
-  } else {    
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
+{       
+  if (both_speeds_avalable){  
+    if (right_motor_speed < 0){  
+      digitalWrite(in3, LOW);
+      digitalWrite(in4, HIGH);
+    } else {    
+      digitalWrite(in3, HIGH);
+      digitalWrite(in4, LOW);
+    }
+    
+    if (left_motor_speed < 0){  
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+    } else { 
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, HIGH);
+    }  
+    analogWrite(enA, left_motor_speed);
+    analogWrite(enB, right_motor_speed); 
   }
   
-  if (right_motor_speed < 0){  
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-  } else { 
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-  }  
-  
-  digitalWrite(inA, left_motor_speed);
-  digitalWrite(inB, right_motor_speed); 
-    
   nh.spinOnce();
 } 
