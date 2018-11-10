f777395ebd7fbfaa20e68bfcd61dabe2a48f7899
Petr Gondek <petrgon@gmail.com>
2018-11-10 23:50:39 +0800

    Abstract class sensor

---
+++ motor_manager/include/sensor.h
@@ -0,0 +1,18 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+
+#include "ros/ros.h"
+#include "wiringPi.h"
+
+class Sensor
+{
+  public:
+    Sensor(int pinNumber, int mode) : this->pinNumber = pinNumber {};
+    virtual ~Sensor = 0;
+
+  protected:    
+    int pinNumber;
+
+};
+
+#endif /*SENSOR_H*/
