94992308e1e36b171eeaedc70e30046cb7822da0
Petr Gondek <petrgon@gmail.com>
2018-11-13 20:53:53 +0800

    Added State

---
+++ motor_manager/include/states/state.h
@@ -0,0 +1,37 @@
+#ifndef STATE_MANAGER_H
+#define STATE_MANAGER_H
+
+class State
+{
+public:
+  State();
+  State(const State &) = default;
+  State(State &&) = default;
+
+  virtual ~State() = 0;
+
+  virtual State &operator=(const State &) = default;
+  virtual State &operator=(State &&) = default;
+
+  virtual bool operator==(const State & b) const;
+
+  virtual void run() = 0;
+
+  virtual void timeElapsedEventHandler();
+
+  virtual void puckAquiredEventHandler();
+
+  virtual void lightDetectedEventHandler();
+  virtual void lightLostEventHandler();
+
+  virtual void leftTouchTriggeredEventHandler();
+  virtual void leftTouchFreedEventHandler();
+
+  virtual void rightTouchTriggeredEventHandler();
+  virtual void rightTouchFreedEventHandler();
+
+  virtual void bothTouchTriggeredEventHandler();
+  virtual void bothtTouchFreedEventHandler();
+
+};
+#endif /*STATE_MANAGER_H*/
