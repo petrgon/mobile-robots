5fc2d3280f0f16fad4e1d3ab38ffbba15bf2990b
Šárka Weberová <webersar@fit.cvut.cz>
2018-11-14 14:24:50 +0800

    motor manager publishing corrected

--- motor_manager/src/managers/motor_manager.cpp
+++ motor_manager/src/managers/motor_manager.cpp
@@ -13,16 +13,15 @@ MotorManager::MotorManager()
 {
     ROS_INFO("Initializing MotorManager");
     ros::NodeHandle node_obj;
-    pub_motor_coords = node_obj.advertise<std_msgs::Int32MultiArray>("/motor_coords", 1);
+    pub_motor_coords = node_obj.advertise<std_msgs::Int32MultiArray>("motor_coords", 1);
     ROS_INFO("MotorManager initialized");
 }
 
 void MotorManager::publishCoords(int left, int right)
 {
-        Coordinates coord = {.left = left, .right = right};
         std_msgs::Int32MultiArray msg;
-        msg.data.push_back(coord.left);
-        msg.data.push_back(coord.right);
+        msg.data.push_back(left);
+        msg.data.push_back(right);
         pub_motor_coords.publish(msg);
         ROS_INFO("Coords published (%d, %d) to /motor_coords ", left, right);
 }
