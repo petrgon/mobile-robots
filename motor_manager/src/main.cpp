e359e0047a1bd222f9ce35032e162ff7d510a69a
Petr Gondek <petrgon@gmail.com>
2018-12-19 20:25:59 +0800

    try to fix segfault

--- motor_manager/src/main.cpp
+++ motor_manager/src/main.cpp
@@ -3,16 +3,22 @@
 #include <signal.h>
 #include "../include/programs/direct_search_puck_program.h"
 
-ProgramManager * programManager = nullptr;
+ProgramManager *programManager = nullptr;
+std::mutex m;
+
 void SIGINT_handler(int sig)
 {
-    delete (programManager);
+
+    std::unique_lock<std::mutex> lck(m);
+    ProgramManager *tmp = programManager;
     programManager = nullptr;
+    delete (tmp);
     ros::shutdown();
+    lck.unlock();
 }
 
 int main(int argc, char **argv)
-{   
+{
     ros::init(argc, argv, "robot_core", ros::init_options::NoSigintHandler);
     signal(SIGINT, SIGINT_handler);
     ROS_INFO("ROS initialized");
@@ -21,15 +27,19 @@ int main(int argc, char **argv)
     ROS_INFO("WiringPI initialized");
 
     int ir_freq = 600;
-    if (argc > 1 )
-	ir_freq = std::stoi(argv[1]);
+    if (argc > 1)
+        ir_freq = std::stoi(argv[1]);
     ROS_INFO("Looking for frequnce %d", ir_freq);
 
-    ProgramManager * programManager = ProgramManager::getInstance();
-    Program * program = new DirectSearchPuckProgram(ir_freq);
+    ProgramManager *programManager = ProgramManager::getInstance();
+    Program *program = new DirectSearchPuckProgram(ir_freq);
     programManager->setProgram(program);
     programManager->start();
-    delete programManager;  //deletes also program
+
+    std::unique_lock<std::mutex> lck(m);
+    ProgramManager *tmp = programManager;
     programManager = nullptr;
+    delete (tmp);
+    lck.unlock();
     return 0;
 }
