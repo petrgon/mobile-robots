0208dbbf3ceb7e1ad067a59882e7eb44171b0ed7
Šárka Weberová <webersar@fit.cvut.cz>
2018-12-19 22:34:04 +0800

    Parametrized direct search constructor dummy implemented

--- motor_manager/src/programs/direct_search_puck_program.cpp
+++ motor_manager/src/programs/direct_search_puck_program.cpp
@@ -54,7 +54,59 @@ DirectSearchPuckProgram::DirectSearchPuckProgram(int irFrequency) : Program(), i
 }
 
 DirectSearchPuckProgram::DirectSearchPuckProgram(int irFrequency, int leftSpeed, int rightSpeed, int time)
-    : Program(), irFrequency(irFrequency) {}
+    : Program(), irFrequency(irFrequency) {
+        
+    State *moveForward = new MoveState(this, leftSpeed, rightSpeed, time);
+    
+    State *dummyStartingState = new MoveState(this, 0, 0, 300);
+    State *searchLeft = new SearchLeftState(this, SEARCHING_TIME);
+    State *lookRight = new SearchRightState(this, ROTATION_TIME);
+    State *turnRight = new MoveState(this, 150, 0, ROTATION_TIME);
+    State *catchLight = new MoveForwardState(this);
+    State *leftCurve = new MoveState(this, CURVE_LEFT_SPEED, CURVE_RIGHT_SPEED, CURVE_TIME);
+    State *endProgram = new ProgramTimeoutState(this);
+    State *moveBack = new MoveBackwardState(this, COLLISION_TIME);
+    State *unexpectedCollision = new MoveBackwardState(this, COLLISION_TIME);
+
+    dummyStartingState->setTimeElapsedNextState(moveForward);
+
+    moveForward->setCollisionNextState(moveBack)->setLightDetectedNextState(catchLight);
+    moveForward->setTimeElapsedNextState(searchLeft);
+
+    moveBack->setTimeElapsedNextState(turnRight);
+
+    turnRight->setTimeElapsedNextState(leftCurve);
+
+    leftCurve->setTimeElapsedNextState(lookRight)->setLightDetectedNextState(catchLight);
+    leftCurve->setCollisionNextState(unexpectedCollision);
+
+    catchLight->setLightLostNextState(lookRight);
+
+    lookRight->setLightDetectedNextState(catchLight)->setTimeElapsedNextState(searchLeft);
+    lookRight->setCollisionNextState(unexpectedCollision);
+
+    searchLeft->setLightDetectedNextState(catchLight)->setCollisionNextState(unexpectedCollision);
+    searchLeft->setTimeElapsedNextState(endProgram);
+
+    unexpectedCollision->setTimeElapsedNextState(endProgram);
+
+    stateConut = 9;
+    allStates = new State *[stateConut];
+    allStates[0] = searchLeft;
+    allStates[1] = lookRight;
+    allStates[2] = moveForward;
+    allStates[3] = catchLight;
+    allStates[4] = leftCurve;
+    allStates[5] = endProgram;
+    allStates[6] = moveBack;
+    allStates[7] = unexpectedCollision;
+    allStates[8] = dummyStartingState;
+
+    actualState = nullptr;
+    nextState = moveForward;
+
+    SensorManager::getInstance()->subscribePuckAquiredEvent(this);
+}
 
 DirectSearchPuckProgram::~DirectSearchPuckProgram()
 {
